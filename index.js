89fe5556c3992179fe0a8f3caeec43786bda4423
2205551130_I Gede Sangga Buana Putra <115202346+DeSangga@users.noreply.github.com>
2024-01-02 20:31:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('( "response": "Halo!!" )');
+  res.send('( "response": "Halo Sangga" )');
 });
 
 app.get("/will", function (req, res) {
-  res.send('( "response": "Selamat Datang!!" )');
+  res.send('( "response": "Selamat Datang Sangga" )');
 });
 app.get("/ready", function (req, res) {
-  res.send('{ "response": "YESSSSSSSS BERHASILLLLLLL" }');
+  res.send('{ "response": "Kamu Keren" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
